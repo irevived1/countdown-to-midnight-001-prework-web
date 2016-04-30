830adb833b5fc66d96f4aedffefb3bbc37a511cc
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-04-30 04:16:11 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(integer = 10)
+  until integer <= 0 do
+    puts "#{integer} SECOND(S)!"
+    integer-=1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+
+
+def countdown_with_sleep(integer = 10)
+  until integer <= 0 do
+    puts "#{integer} SECOND(S)!"
+    sleep(1)
+    integer-=1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
